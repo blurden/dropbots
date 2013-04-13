3a2ffb7b6a514913bb8e76dc339b89ad14255be7
blurden <blurden@gmail.com>
2013-04-13 04:17:28 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropbots::Application.config.secret_token = '890ab4e095b3261ad7f04fdb70ea2d9bd3898bd32cb62d70847aaced7d6a1977394ca1b9edacbbfa955068a0af1e9a90d68e209eb4b7e00f17649b30e856e43c'
